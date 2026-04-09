0b6882e493f70cbdf0f8f56515ac80dab0414b82
guanchendu <3382006114@qq.com>
2026-04-09 18:50:02 +0800

    datasts

--- Background/4.8_ViT.py
+++ Background/4.8_ViT.py
@@ -2,6 +2,11 @@ import torch
 import torch.nn as nn
 
 
+
+
+
+
+
 class PatchEmbed(nn.Module):
     def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=192):
         super().__init__()
